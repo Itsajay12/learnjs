94904c029c54ff2745d43239d08760a047cfc03a
itsajay12 <vajay570040@gmail.com>
2023-11-08 14:57:23 +0530

    program to find factorial

--- external.js
+++ external.js
@@ -1,8 +1,10 @@
-function change(){
-    document.getElementById("p1").innerHTML="Its changed";
-    document.getElementById("h5").style.fontFamily="cursive";
-    document.getElementById("h5").style.fontSize="20px";
+function change() {
+    document.getElementById("p1").innerHTML = "Its changed";
+    document.getElementById("h5").style.fontFamily = "cursive";
+    document.getElementById("h5").style.fontSize = "20px";
+    console.log("cahnged called");
+    alert("function called");
 }
-function img_change(){
-    document.getElementById("img").src="https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/01-bmw-i7-rt-2023-lead-driving.jpg?itok=CZEpiym_"
+function img_change() {
+    document.getElementById("img").src = "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/01-bmw-i7-rt-2023-lead-driving.jpg?itok=CZEpiym_"
 }
