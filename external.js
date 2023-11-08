9cec6ffa72fb3be1a50e703f9409007fdf7db146
itsajay12 <vajay570040@gmail.com>
2023-11-08 14:37:53 +0530

    fundamentals

---
+++ external.js
@@ -0,0 +1,8 @@
+function change(){
+    document.getElementById("p1").innerHTML="Its changed";
+    document.getElementById("h5").style.fontFamily="cursive";
+    document.getElementById("h5").style.fontSize="20px";
+}
+function img_change(){
+    document.getElementById("img").src="https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/01-bmw-i7-rt-2023-lead-driving.jpg?itok=CZEpiym_"
+}
